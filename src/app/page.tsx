8f0fb1e06baea790c6417a5f928dc5bcb8bacab2
limbuthitoo <bijaysubbalimbu@gmail.com>
2026-03-31 01:19:07 +0545

    remove: scroll marquee text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import BentoGrid from "@/components/bento/BentoGrid";
 import HeroSection from "@/components/bento/HeroSection";
-import ScrollMarquee from "@/components/common/ScrollMarquee";
 import { getProjects, getExperiences, getSiteConfig, seedIfEmpty } from "@/lib/db";
 
 export const revalidate = 60;
@@ -11,14 +10,9 @@ export default async function Home() {
   const experiences = await getExperiences();
   const siteConfig = await getSiteConfig();
 
-  const marqueeTexts = siteConfig.roles?.length
-    ? siteConfig.roles
-    : ["Design Engineer", "Frontend Developer", "UI/UX Designer", "Creative Technologist"];
-
   return (
     <>
       <HeroSection siteConfig={siteConfig} />
-      <ScrollMarquee texts={marqueeTexts} />
       <BentoGrid projects={projects} experiences={experiences} siteConfig={siteConfig} />
     </>
   );
